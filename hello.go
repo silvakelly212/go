35c31771212c5657f4c42c06955de2c6c19d493d
silvakelly212 <silvakelly212@gmail.com>
2025-07-28 12:51:13 -0300

    Update hello.go
    Testando multiplas vezes // inserindo constante e importando pacote de tempo

--- hello.go
+++ hello.go
@@ -321,3 +321,77 @@ func iniciarMonitoramento() {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
 	}
 }
+
+// Craindo constante para implementacao do tempo de execucao
+
+package main
+import (
+	"fmt"
+	"net/http"
+	"time"
+        "os"
+	)
+const monitoramento = 3
+const delay = 5
+
+func main() {
+	exibeIntroduvao()
+	exibeMenu()
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		iniciarMonitoramento()
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
+func exibeIntroduvao() {
+	nome := "Kelly"
+	versao := 1.1
+	fmt.Println("Olá sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	return comandoLido
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do Programa")
+
+}
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := []string{"https:www.caelum.com", "https:www.alura.com.br"}
+	for i := 0; i < monitoramento; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+		}
+	fmt.Println("")
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
+}
